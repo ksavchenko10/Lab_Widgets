62187ae221fd0bd64b04da82a86251dc663ea716
Kris <Valentina10@sibmail.com>
2022-04-28 02:00:35 +0700

    Comments on the file area.h

--- Widget/area.h
+++ Widget/area.h
@@ -7,16 +7,16 @@ class Area : public QWidget
     int myTimer; // идентификатор таймера
     float alpha; // угол поворота
 public:
-    Area(QWidget *parent = 0);
-    ~Area();
-    MyLine *myline;
-    MyRect *myrect;
+    Area(QWidget *parent = 0); // конструктор
+    ~Area(); // деструктор
+    MyLine *myline; // линия
+    MyRect *myrect; // квадрат
 protected:
 // обработчики событий
-    void paintEvent(QPaintEvent *event);
-    void timerEvent(QTimerEvent *event);
-    void showEvent(QShowEvent *event);
-    void hideEvent(QHideEvent *event);
+    void paintEvent(QPaintEvent *event); // перерисовываем фигуры с новым углом
+    void timerEvent(QTimerEvent *event); // события таймера для изменения угла
+    void showEvent(QShowEvent *event); // создание таймера
+    void hideEvent(QHideEvent *event); // уничтожение таймера
 };
 
 #endif // AREA_H
