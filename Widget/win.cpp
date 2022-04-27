04d11d5149b8e5cbb8934067a8f1466437baf542
Kris <Valentina10@sibmail.com>
2022-04-28 00:21:08 +0700

    Comments on win.cpp file

--- Widget/win.cpp
+++ Widget/win.cpp
@@ -15,24 +15,24 @@ Win::Win(QWidget *parent):QWidget(parent)
  calcbutton=new QPushButton("+1",this);
  exitbutton=new QPushButton(codec->toUnicode("Выход"),this);
 
- QHBoxLayout *layout1 = new QHBoxLayout();
- layout1->addWidget(label1);
- layout1->addWidget(label2);
- QHBoxLayout *layout2 = new QHBoxLayout();
- layout2->addWidget(edit1);
- layout2->addWidget(edit2);
- QHBoxLayout *layout3 = new QHBoxLayout();
- layout3->addWidget(calcbutton);
- layout3->addWidget(exitbutton);
- QVBoxLayout *layout4 = new QVBoxLayout(this);
- layout4->addLayout(layout1);
- layout4->addLayout(layout2);
- layout4->addLayout(layout3);
+ QHBoxLayout *layout1 = new QHBoxLayout(); // создание менеджера компоновки для управления размещением меток
+ layout1->addWidget(label1); // метка "счет по 1"
+ layout1->addWidget(label2); // метка "счет по 5"
+ QHBoxLayout *layout2 = new QHBoxLayout(); // создание менеджера компоновки для управления размещением элементов ввода вывода
+ layout2->addWidget(edit1); // поле ввода "счет по 1"
+ layout2->addWidget(edit2); // поле ввода "счет по 5"
+ QHBoxLayout *layout3 = new QHBoxLayout(); // создание менеджера компоновки для управления размещением кнопок
+ layout3->addWidget(calcbutton); // кнопка +1
+ layout3->addWidget(exitbutton); // кнопка выход
+ QVBoxLayout *layout4 = new QVBoxLayout(this); // создание менеджера компоновки для управления размещением других менеджеров покмпоновок
+ layout4->addLayout(layout1); // добавление компоновщика меток к общему компоновщику
+ layout4->addLayout(layout2); // добавление компоновщика полей ввода к общему компоновщику
+ layout4->addLayout(layout3); // добавление компоновщика кнопок к общему компоновщику
  // связь сигнала нажатия кнопки и слота закрытия окна
  connect(calcbutton,SIGNAL(clicked(bool)),
- edit1,SLOT(add_one()));
+ edit1,SLOT(add_one())); // связь сигнала нажатия кнопки "+1" и вызова метода add_one
  connect(edit1,SIGNAL(tick_signal()),
- edit2,SLOT(add_one()));
+ edit2,SLOT(add_one())); // связь сигнала tick_signal для прибавления поля "счет по 5"
  connect(exitbutton,SIGNAL(clicked(bool)),
- this,SLOT(close()));
+ this,SLOT(close())); // связь сигнала нажатия кнопки и слота закрытия окна
 }
