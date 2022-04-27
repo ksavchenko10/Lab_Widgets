687fc066d982dafb4562961279784f5408ba2222
Kris <Valentina10@sibmail.com>
2022-04-28 02:24:28 +0700

    Comments on the file window.cpp

--- Widget/window.cpp
+++ Widget/window.cpp
@@ -6,9 +6,9 @@ Window::Window()
     codec = QTextCodec::codecForName("Windows-1251");
     this->setWindowTitle(codec->toUnicode("Обработка событий"));
     area = new Area( this );
-    btn = new QPushButton(codec->toUnicode("Завершить"),this );
-    QVBoxLayout *layout = new QVBoxLayout(this);
-    layout->addWidget(area);
-    layout->addWidget(btn);
-    connect(btn, SIGNAL(clicked(bool)),this,SLOT(close()));
+    btn = new QPushButton(codec->toUnicode("Завершить"),this ); // создание кнопки завершить
+    QVBoxLayout *layout = new QVBoxLayout(this); // создаем компановщик
+    layout->addWidget(area); // добавляем холст к компановщику
+    layout->addWidget(btn); // добавляем кнопку к компановщику
+    connect(btn, SIGNAL(clicked(bool)),this,SLOT(close())); // связываем события нажатия кнопки выход
 };
