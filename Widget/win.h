3ccb4867165e54549fece3adcc7730792470bd8c
Kris <Valentina10@sibmail.com>
2022-04-28 00:09:08 +0700

    Comments on win.h file

--- Widget/win.h
+++ Widget/win.h
@@ -11,35 +11,35 @@
 #include <QTextCodec>
 
 
-class Counter:public QLineEdit
+class Counter:public QLineEdit // класс поля ввода
 {
- Q_OBJECT
+ Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
 public:
- Counter(const QString & contents, QWidget *parent=0):
+ Counter(const QString & contents, QWidget *parent=0): // конструктор
  QLineEdit(contents,parent){}
 signals:
- void tick_signal();
+ void tick_signal(); // добавляем сигнал tick_signal
 public slots:
- void add_one()
+ void add_one() // метод прибавляющий счетчик при нажатии кнопки +1
  {
- QString str=text();
- int r=str.toInt();
- if (r!=0 && r%5 ==0) emit tick_signal();
- r++;
- str.setNum(r);
- setText(str);
+ QString str=text(); // берем текст из поля "счет по 1"
+ int r=str.toInt(); // переводим в число
+ if (r!=0 && r%5 ==0) emit tick_signal(); // если произошло 5 щелчков, отправляем сигнал чтобы увеличит на 1 поле "счет по 5"
+ r++; // прибавляем счетчик
+ str.setNum(r); // переводим число в строку
+ setText(str); // записываем в поле "счет по 1" новое число
  }
 };
 class Win: public QWidget
 {
  Q_OBJECT
 protected:
- QTextCodec *codec;
- QLabel *label1,*label2;
- Counter *edit1,*edit2;
- QPushButton *calcbutton;
- QPushButton *exitbutton;
+ QTextCodec *codec; // для отображения кириллицы
+ QLabel *label1,*label2; // метка ввода
+ Counter *edit1,*edit2; // счетчик
+ QPushButton *calcbutton; // кнопка +1
+ QPushButton *exitbutton; // кнопка Выход
 public:
- Win(QWidget *parent = 0);
+ Win(QWidget *parent = 0); // конструктор
 };
 #endif // WIN_H
