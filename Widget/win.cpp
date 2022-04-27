0a81446a582609b79fb79ae7c45702b9387ec3e1
Kris <Valentina10@sibmail.com>
2022-04-27 23:44:31 +0700

    Program code

--- Widget/win.cpp
+++ Widget/win.cpp
@@ -6,80 +6,33 @@
 
 Win::Win(QWidget *parent):QWidget(parent)
 {
-     codec = QTextCodec::codecForName("UTF-8");
-     setWindowTitle(codec->toUnicode("Возведение в квадрат"));
-     frame = new QFrame(this);
-     frame->setFrameShadow(QFrame::Raised);
-     frame->setFrameShape(QFrame::Panel);
-     inputLabel = new QLabel(codec->toUnicode("Введите число:"),
-     this);
-     inputEdit = new QLineEdit("",this);
-     StrValidator *v=new StrValidator(inputEdit);
-     inputEdit->setValidator(v);
-     outputLabel = new QLabel(codec->toUnicode("Результат:"),
-     this);
-     outputEdit = new QLineEdit("",this);
-     nextButton = new QPushButton(codec->toUnicode("Следующее"),
-     this);
-     exitButton = new QPushButton(codec->toUnicode("Выход"),
-     this);
-     // компоновка приложения выполняется согласно рисунку 2.
-     QVBoxLayout *vLayout1 = new QVBoxLayout(frame); // создание менеджера компоновки для управления размещением элементов ввода вывода в окне win
-     vLayout1->addWidget(inputLabel); // добавление метки ввода к компоновщику
-     vLayout1->addWidget(inputEdit); // добавление формы ввода к компоновщику
-     vLayout1->addWidget(outputLabel); // добавление метки вывода к компоновщику
-     vLayout1->addWidget(outputEdit); // добавление формы вывода к компоновщику
-     vLayout1->addStretch(); //"Пружина", поджатие элементов друг к другу
-     QVBoxLayout *vLayout2 = new QVBoxLayout();  // создание менеджера компоновки для управления размещением кнопок в окне win
-     vLayout2->addWidget(nextButton);  // добавление кнопки "следующий" к компоновщику
-     vLayout2->addWidget(exitButton); // добавление кнопки "выход" к компоновщику
-     vLayout2->addStretch(); //"Пружина", поджатие элементов друг к другу
-     QHBoxLayout *hLayout = new QHBoxLayout(this); //создание менеджера компоновки для управления фреймом в окне win
-     hLayout->addWidget(frame); //добавление фрейма к компоновщику
-     hLayout->addLayout(vLayout2); //добавление компоновщика кнопок к компоновику фрейма
-     begin();
-     connect(exitButton,SIGNAL(clicked(bool)),
-     this,SLOT(close())); // закрытие окна на нажатие кнопки выход
-     connect(nextButton,SIGNAL(clicked(bool)),
-     this,SLOT(begin())); // вызов метода begin на нажатие кнопки следующий
-     connect(inputEdit,SIGNAL(returnPressed()), // вызов метода  calc на завершение ввода в поле ввода "введите число"
-     this,SLOT(calc()));
-}
-void Win::begin()
-{
-     inputEdit->clear(); // очистить поле ввода
-     nextButton->setEnabled(false); // кнопку следующий сделать неактивной
-     nextButton->setDefault(false); // сделать неактивной по умолчанию, чтобы при нажатии на enter кнопка не активировалась
-     inputEdit->setEnabled(true); // поле ввода числа сделать неактивным
-     outputLabel->setVisible(false); // поле метки вывода результата скрыть
-     outputEdit->setVisible(false); // поле вывода результата скрыть
-     outputEdit->setEnabled(false); // поле вывода результата сделать неактивным
-     inputEdit->setFocus(); // установить фокус в поле ввода числа
-}
-void Win::calc() // запускается после окончания вводе числа
-{
-     bool Ok=true; float r,a;
-     QString str=inputEdit->text();
-     a=str.toDouble(&Ok); // переводим введенную строку в числовой формат
-     if (Ok) // если число введено
-     {
-     r=a*a; // возводим в квадрат
-     str.setNum(r); // переводим число в строку и приправаем к переменной str
-     outputEdit->setText(str); // в поле вывода результат вводим получившийся результат
-     inputEdit->setEnabled(false); // поле ввода числа делаем неактивным
-     outputLabel->setVisible(true); // показываем метку поля вывода резльтата
-     outputEdit->setVisible(true); // показываем  поле вывода резльтата
-     nextButton->setDefault(true); // делаем кнопку следующий активной по умолчанию
-     nextButton->setEnabled(true); // делаем кнопку следующий активной
-     nextButton->setFocus(); // устанавливаем фокус на кнопке следующий
-     }
-     else
-     if (!str.isEmpty()) // если введенная строка не пустая и не равна числу
-     {
-     QMessageBox msgBox(QMessageBox::Information,  // создаем окно уведомления
-     codec->toUnicode("Возведение в квадрат."), // название окна
-     codec->toUnicode("Введено неверное значение."), // текст окна
-     QMessageBox::Ok); // добавляем кнопку ОК
-     msgBox.exec(); // вызов окна с уведомлением
-     }
+ codec = QTextCodec::codecForName("Windows-1251");
+ this->setWindowTitle(codec->toUnicode("Счетчик"));
+ label1 = new QLabel(codec->toUnicode("Cчет по 1"),this);
+ label2 = new QLabel(codec->toUnicode("Cчет по 5"),this);
+ edit1 = new Counter("0",this);
+ edit2 = new Counter("0",this);
+ calcbutton=new QPushButton("+1",this);
+ exitbutton=new QPushButton(codec->toUnicode("Выход"),this);
+
+ QHBoxLayout *layout1 = new QHBoxLayout();
+ layout1->addWidget(label1);
+ layout1->addWidget(label2);
+ QHBoxLayout *layout2 = new QHBoxLayout();
+ layout2->addWidget(edit1);
+ layout2->addWidget(edit2);
+ QHBoxLayout *layout3 = new QHBoxLayout();
+ layout3->addWidget(calcbutton);
+ layout3->addWidget(exitbutton);
+ QVBoxLayout *layout4 = new QVBoxLayout(this);
+ layout4->addLayout(layout1);
+ layout4->addLayout(layout2);
+ layout4->addLayout(layout3);
+ // связь сигнала нажатия кнопки и слота закрытия окна
+ connect(calcbutton,SIGNAL(clicked(bool)),
+ edit1,SLOT(add_one()));
+ connect(edit1,SIGNAL(tick_signal()),
+ edit2,SLOT(add_one()));
+ connect(exitbutton,SIGNAL(clicked(bool)),
+ this,SLOT(close()));
 }
--- Widget/win.h
+++ Widget/win.h
@@ -11,31 +11,35 @@
 #include <QTextCodec>
 
 
-class Win:public QWidget // класс окна
+class Counter:public QLineEdit
 {
- Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
-protected:
- QTextCodec *codec; //для отображения кириллицы
- QFrame *frame; // рамка
- QLabel *inputLabel; // метка ввода
- QLineEdit *inputEdit; // строчный редактор ввода
- QLabel *outputLabel; // метка вывода
- QLineEdit *outputEdit; // строчный редактор вывода
- QPushButton *nextButton; // кнопка Следующее
- QPushButton *exitButton; // кнопка Выход
+ Q_OBJECT
 public:
- Win(QWidget *parent = 0); // конструктор
+ Counter(const QString & contents, QWidget *parent=0):
+ QLineEdit(contents,parent){}
+signals:
+ void tick_signal();
 public slots:
- void begin(); // метод начальной настройки интерфейса
- void calc(); // метод реализации вычислений
-};
-class StrValidator:public QValidator // класс компонента проверки ввода
-{
-public:
- StrValidator(QObject *parent):QValidator(parent){}
- virtual State validate(QString &str,int &pos)const
+ void add_one()
  {
- return Acceptable; // метод всегда принимает вводимую строку
+ QString str=text();
+ int r=str.toInt();
+ if (r!=0 && r%5 ==0) emit tick_signal();
+ r++;
+ str.setNum(r);
+ setText(str);
  }
+};
+class Win: public QWidget
+{
+ Q_OBJECT
+protected:
+ QTextCodec *codec;
+ QLabel *label1,*label2;
+ Counter *edit1,*edit2;
+ QPushButton *calcbutton;
+ QPushButton *exitbutton;
+public:
+ Win(QWidget *parent = 0);
 };
 #endif // WIN_H
