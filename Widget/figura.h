e4eecf990605133c413c4e67349cf3dd533bd82a
Kris <Valentina10@sibmail.com>
2022-04-28 01:53:36 +0700

    Comments on the file figura.h

--- Widget/figura.h
+++ Widget/figura.h
@@ -6,25 +6,25 @@
 class Figura
 {
 protected:
-    int x,y,halflen,dx,dy,r;
-    virtual void draw(QPainter *Painter)=0;
+    int x,y,halflen,dx,dy,r; // параметры фигуры
+    virtual void draw(QPainter *Painter)=0; // перерисовка
 public:
-    Figura(int X,int Y,int Halflen):
+    Figura(int X,int Y,int Halflen): // конструктор
                                  x(X),y(Y),halflen(Halflen){}
-    void move(float Alpha,QPainter *Painter);
+    void move(float Alpha,QPainter *Painter); // пересчитывания угла и вызов перерисовки
 };
-class MyLine:public Figura
+class MyLine:public Figura // класс линии
 {
 protected:
     void draw(QPainter *Painter);
 public:
-    MyLine(int x,int y,int halflen):Figura(x,y,halflen){}
+    MyLine(int x,int y,int halflen):Figura(x,y,halflen){} // конструктор
 };
-class MyRect:public Figura
+class MyRect:public Figura // класс квадрата
 {
 protected:
     void draw(QPainter *Painter);
 public:
-    MyRect(int x,int y,int halflen):Figura(x,y,halflen){}
+    MyRect(int x,int y,int halflen):Figura(x,y,halflen){} // конструктор
 };
 #endif // FIGURA_H
