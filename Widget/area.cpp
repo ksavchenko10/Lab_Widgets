360419f903425e664e135cadd9419fae1bf5c3dd
Kris <Valentina10@sibmail.com>
2022-04-28 02:15:43 +0700

    Comments on the file area.cpp

--- Widget/area.cpp
+++ Widget/area.cpp
@@ -4,26 +4,26 @@
 Area::Area(QWidget *parent):QWidget(parent)
 {
      setFixedSize(QSize(300,200));
-     myline=new MyLine(80,100,50);
-     myrect=new MyRect(220,100,50);
-     alpha=0;
+     myline=new MyLine(80,100,50); // создаем линию
+     myrect=new MyRect(220,100,50); // создаем квадрат
+     alpha=0; // угол
 }
 void Area::showEvent(QShowEvent *)
 {
      myTimer=startTimer(50); // создать таймер
 }
-void Area::paintEvent(QPaintEvent *)
+void Area::paintEvent(QPaintEvent *) // перерисовываем фигуры с новым углом
 {
     QPainter painter(this);
-    painter.setPen(Qt::red);
-    myline->move(alpha,&painter);
-    myrect->move(alpha*(-0.5),&painter);
+    painter.setPen(Qt::red); // задаем красный цвет
+    myline->move(alpha,&painter); // перерисовываем линию с новым углом
+    myrect->move(alpha*(-0.5),&painter); // перерисовываем квадрат с новым углом
 }
 void Area::timerEvent(QTimerEvent *event)
 {
     if (event->timerId() == myTimer) // если наш таймер
     {
-       alpha=alpha+0.2;
+       alpha=alpha+0.2; // прибавить угол
        update(); // обновить внешний вид
     }
     else
@@ -34,8 +34,8 @@ void Area::hideEvent(QHideEvent *)
 {
      killTimer(myTimer); // уничтожить таймер
 }
-Area::~Area()
+Area::~Area() // деструктор
 {
-    delete myline;
-    delete myrect;
+    delete myline; // уничтожаем  myline
+    delete myrect; // уничтожаем  myrect
 }
